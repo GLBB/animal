d4d4dc90118c7eba16325dfebb4cc69de5ddbe72
gl <unbeatableglbb@outlook.com>
2019-03-06 21:34:56 +0800

    :lipstick: 2019/03/06 增加简单首页

---
+++ src/main/java/cn/gl/share_knowledge/controller/IndexController.java
@@ -0,0 +1,13 @@
+package cn.gl.share_knowledge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "public/index";
+    }
+}
